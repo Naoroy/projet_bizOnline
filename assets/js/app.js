61efe0ac364e49b71d371078d47af0b4fe9b4fd7
Naoroy <fegan.pierre@gmail.com>
2017-11-13 12:53:10 +0100

    ajout IIFE, ajout fonction pour récup input.value, ajout onclick sur button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,42 @@
+/**@namespace */
+const gestionStock = (function() {
+    var stock = [];
+    var inputName, inputPrice, inputDescription;
+    window.onload = function init() {
+        getProductInfo();
+        document.querySelector("button").onclick = createProduct;
+    };
+    /**
+     * @author Bernard Salah
+     * @constructor
+     * @alias gestionStock.Produits
+     * @param {string} nom
+     * @param {number} prix
+     * @param {string} description
+     */
+    const Produits = function(nom, prix, description) {
+        this.nom = nom;
+        this.prix = prix;
+        this.description = description;
+    };
 
-/**
- * @author Bernard Salah
- * @param {string} nom
- * @param {number} prix
- * @param {string} description
- */
-const produits = function(nom, prix, description){
-  this.nom = nom;
-  this.prix = prix;
-  this.description = description;
-}
-
-/**
- * @author Fégan PIERRE
- * @param {string} nom
- * @param {number} prix
- * @param {string} description
- * @returns {object} stock
- */
-const createProduct = function (nom, prix, description) {
-    let newProduct = new produits(nom, prix, description);
-    stock.push(newProduct);
-    console.log(stock);
-    return stock;
-}
+    /**
+     * @author Fégan PIERRE
+     * @alias gestionStock.createProduct
+     * @description Crée un nouveau produits avec les valeurs (input)
+     */
+    const createProduct = function () {
+        let newProduct = new Produits(inputName, inputPrice, inputDescription);
+        stock.push(newProduct);
+        console.log(stock);
+    };
+    /**
+     *@description Récupère les valeurs (input)
+     @alias gestionStock.getProductInfo
+    */
+    const getProductInfo = function () {
+        inputName = document.querySelector("input#my_name").value;
+        inputPrice = document.querySelector("input#my_price").value;
+        inputDescription = document.querySelector("input#my_description").value;
+    };
+}());
